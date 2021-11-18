aa6ae80d3bccc4cc8d61f60950993829bc9082f3
Soham Chowdhury <77236783+code-soham@users.noreply.github.com>
2021-11-19 02:10:49 +0530

    social

--- src/comp/Footer/Footer.js
+++ src/comp/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Contact from "./ContactMe";
 import Social from "./Social";
+import { Divider, Typography } from "@mui/material";
 export default function Footer() {
   return (
     <React.Fragment>
@@ -8,9 +9,16 @@ export default function Footer() {
         <div className="foot-l-div">
           <Contact />
         </div>
+        <Divider orientation="vertical" flexItem />
         <div className="foot-r-div">
-        <Social />
+          <Social />
         </div>
+      </div>
+      <div className="footer2">
+        <Typography variant="body2" align="center" lineHeight="1em">
+          Designed and developed by Soham Chowdhury ©2021 <br></br>
+          Licensed under the MIT License
+        </Typography>
       </div>
       {/* </div> */}
     </React.Fragment>
--- src/comp/Footer/Social.js
+++ src/comp/Footer/Social.js
@@ -1,12 +1,60 @@
-import { Typography } from "@mui/material";
+import { Typography, Tooltip } from "@mui/material";
 import React from "react";
+import {
+  FacebookTwoTone,
+  Instagram,
+  LinkedIn,
+  Twitter,
+  GitHub,
+} from "@mui/icons-material";
 export default function Social() {
+  const iconsData = [
+    {
+      icon: <LinkedIn fontSize="large" />,
+      link: "https://www.linkedin.com/",
+      name: "LinkedIn",
+    },
+    {
+      icon: <Twitter fontSize="large" />,
+      link: "https://www.twitter.com/",
+      name: "Twitter",
+    },
+    {
+      icon: <GitHub fontSize="large" />,
+      link: "https://www.github.com/",
+      name: "GitHub",
+    },
+    {
+      icon: <FacebookTwoTone fontSize="large" />,
+      link: "https://www.facebook.com/",
+      name: "Facebook",
+    },
+    {
+      icon: <Instagram fontSize="large" />,
+      link: "https://www.instagram.com/",
+      name: "Instagram",
+    },
+  ];
   return (
     <React.Fragment>
       <div className="social-div">
-        <Typography variant="h4">
-          Find Me
-        </Typography>
+        <Typography variant="h4">Connect With Me</Typography>
+        <div className="social-links">
+          {iconsData.map((icon, index) => (
+            <Tooltip title={icon.name} key={index}>
+              <a
+                style={{ textDecoration: "none", color: "inherit" }}
+                href={icon.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={index}
+                className="zoomcon"
+              >
+                {icon.icon}
+              </a>
+            </Tooltip>
+          ))}
+        </div>
       </div>
     </React.Fragment>
   );
