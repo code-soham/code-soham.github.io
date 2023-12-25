6b0f76499fc1c65639866539a5327b76bcf698a9
code-soham <soham36chowdhury@gmail.com>
2023-12-26 02:42:23 +0530

    output folder deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //add imgur
+  output: "export",
   images: {
-    domains: ["i.imgur.com","camo.githubusercontent.com","loremflickr.com"],
+    domains: ["i.imgur.com", "camo.githubusercontent.com", "loremflickr.com"],
   },
 };
 
